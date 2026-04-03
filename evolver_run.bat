b7e7b698d7a578e909da0254e176cc18d2b51658
shh32010 <3201069521@QQ.COM>
2026-04-03 18:10:13 +0800

    chore: remove .bat and .ps1 screenshot scripts from repository

--- evolver_run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set EVOLVER_USE_PARENT_GIT=true
-set EVOLVER_ROLLBACK_MODE=stash
-set EVOLVE_STRATEGY=repair-only
-node "C:\Users\002\.openclaw\workspace\skills\capability-evolver\index.js" run --review
