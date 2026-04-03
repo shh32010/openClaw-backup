19c08a9f5cfe1e6f5daa875282eff4de172f5fef
shh32010 <3201069521@QQ.COM>
2026-04-03 18:11:33 +0800

    chore: restore evolver_run.bat

---
+++ evolver_run.bat
@@ -0,0 +1,5 @@
+﻿@echo off
+set EVOLVER_USE_PARENT_GIT=true
+set EVOLVER_ROLLBACK_MODE=stash
+set EVOLVE_STRATEGY=repair-only
+node "C:\Users\002\.openclaw\workspace\skills\capability-evolver\index.js" run --review
